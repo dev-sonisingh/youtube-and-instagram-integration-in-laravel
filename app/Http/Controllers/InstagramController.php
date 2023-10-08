86c32895d4de3d8607b3146abb40646d4640a264
dev-sonisingh <82632726+dev-sonisingh@users.noreply.github.com>
2023-10-08 22:19:48 +0530

    Update InstagramController.php

--- app/Http/Controllers/InstagramController.php
+++ app/Http/Controllers/InstagramController.php
@@ -8,32 +8,12 @@ use Illuminate\Support\Facades\Http;
 
 class InstagramController extends Controller
 {
-    // public function fetchPosts()
-    // {
-    //     // Replace with your Instagram access token and user ID
-    //     $accessToken = 'IGQWRNM19SZAUtvUFJWOEJpU1FiS1NIeTJQcnc1Mi1jZAzByQU9vUV8zbFJVTzdjSkhNZAUt6d0dlaS1Kb1BEdmczaXVXYS1HeTBITFZAXbXhXX19IdkNjU2duYXEzUW1YTmlxdm5vc0thVjBiSXhiWVdmZAFFidktlbUEZD';
-    //     $userId = 'codingdemon_';
-
-    //     // Make a GET request to the Instagram Graph API
-    //     $response = Http::get("https://graph.instagram.com/v12.0/{$userId}/media?fields=id,caption,media_type,media_url,permalink,timestamp&access_token={$accessToken}");
-
-    //     // Check for a successful response
-    //     if ($response->successful()) {
-    //         $instagramPosts = $response->json()['data'];
-
-    //         // Process and pass the Instagram posts to a view
-    //         return view('instagram-posts', ['instagramPosts' => $instagramPosts]);
-    //     } else {
-    //         // Handle the API request error
-    //         return 'Error fetching Instagram posts';
-    //     }
-    // }
-    // //
+ 
 
     public function fetchFeeds()
     {
         // Replace with your Instagram access token
-        $accessToken = 'IGQWRPUkl6RFpueG1Fd0cxSWV5ME5ScnJ4RmVyVkNvZATN3WlBoa1F4aEFid1RZAeDctOTlxd1J1ejdOWnhqQ0VZAWEJvaVdJZAElwbi1DNFd2bjJEdkF4Nk1SRGFwaE81aUpqV1FFcndpUENhOXZAmMGM1S25hMUlwTE0ZD';
+        $accessToken = 'your_access_token';
 
         // Make a GET request to the Instagram Graph API
         $limit = 3;
